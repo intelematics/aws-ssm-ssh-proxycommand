3ad1ff1241e292f919100892585b540809cff8f8
Nick Doyle <nick.doyle@intelematics.com>
2020-03-04 14:36:40 +1100

    initial

---
+++ README.md
@@ -0,0 +1,178 @@
+# AWS SSM SSH ProxyCommand
+
+A script to
+
+  - Install & configure prerequisites for itself with as little user interaction as possible
+  - Be used as an [SSH ProxyCommand](https://man.openbsd.org/ssh_config.5#ProxyCommand), in order to easily SSH to an EC2 Instance (via SSM)
+
+Once you're connected with SSH you can do all normal SSH things
+- Port forwarding e.g. to RDS - most useful since [SSM does not yet natively support port forwarding to a non-local IP, see GitHub issue 208](https://github.com/aws/amazon-ssm-agent/issues/208)
+- [SCP](https://en.wikipedia.org/wiki/Secure_copy) to copy files (also not yet natively supported by SSM)
+- [SOCKS Proxy](https://www.digitalocean.com/community/tutorials/how-to-route-web-traffic-securely-without-a-vpn-using-a-socks-tunnel)
+- Interactive Bash Terminal etc.
+
+Should work for Windows (with WSL), OSX & Linux clients
+
+![ssh over ssm architecture](doc/aws-ssh-over-ssm.png)
+
+# Quick Install
+
+After you have briefly reviewed the script for malicious code, execute it like so:
+
+`curl -s 'https://raw.githubusercontent.com/intelematics/infra-utils/master/aws/aws-ssm-ec2-proxy-command.sh?token=AAI4XFIE6EUNRK26I4I5QT26JJA5Y' | bash`
+
+# Usage
+
+Once Jumpboxes are set up in your environment, and your client is set up with this script, you will be able to simply and securely start an SSH session to EC2 instances using their instance IDs e.g. `ssh ubuntu@i-xxxxxxxx`
+
+If your jumpbox instance is named `jumpbox`, you can do just `ssh jumpbox` and the script will find that instance and connect.
+
+## Usage Examples
+
+Goal|Command
+-|-
+Port forward to RDS | `ssh -N -L 1433:tsh.cunh7nydpqk3.ap-southeast-2.rds.amazonaws.com:1433 ubuntu@jumpbox`
+Port forward to a private ALB | `ssh -N -L 8443:wso2-analytics-lb-96ed1e73216b9ef5.elb.ap-southeast-2.amazonaws.com:443 ubuntu@jumpbox`
+SOCKS Proxy over SSH | `ssh -N -D 31337 i-0ce3943048e94f202`
+| `ssh -N -D 31337 jumpbox`
+
+
+## Jumpboxes Requirements
+
+- Have the [SSM Agent](https://docs.aws.amazon.com/systems-manager/latest/userguide/ssm-agent.html) installed (comes preinstalled on all Intelematics SOE AMIs, and all Amazon Linux AMIs)
+
+- Have an Instance Profile that contains policy
+    [AmazonSSMManagedInstanceCore](https://console.aws.amazon.com/iam/home?#/policies/arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore) `arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore`
+
+- Permit outbound traffic to either
+  - Internet & [SSM Service Endpoint](https://docs.aws.amazon.com/general/latest/gr/ssm.html) **or**
+  - [SSM VPCE that you optionally set up to keep traffic within your VPC](https://docs.aws.amazon.com/systems-manager/latest/userguide/setup-create-vpc.html) (so the agent can phone home)
+
+### Highly Recommended Jumpbox Config
+
+- Be in private subnets (no ingress from public internet) and
+- Deny all inbound traffic
+
+## Ensuring SSM Agent is the Latest Version
+
+Create an [SSM Association](https://docs.aws.amazon.com/systems-manager/latest/userguide/sysman-state-assoc.html) to regularly update the SSM Agent on all managed instances, every Sunday @ 2am
+
+`aws ssm create-association --name AWS-UpdateSSMAgent --schedule-expression "cron(0 2 ? * SUN *)" --targets Key=instanceids,Values='*'`
+
+Or via Terraform, create an association for continuous auto-updates
+
+1. Get ref to the `AWS-UpdateSSMAgent` document https://www.terraform.io/docs/providers/aws/d/ssm_document.html
+1. Create the Association
+https://www.terraform.io/docs/providers/aws/r/ssm_association.html
+
+## Security and Access Control
+With SSM, security now becomes controlled via IAM policy allowing a Role the ability to execute SSM Documents.
+
+Initial (easy) implementation might give all Developers SSM:* on all EC2 instances.
+
+### Advanced Security and Access Control
+
+You may then want to lock this down further (which would be needed in order to pass an AWS Well Architected Review), to limit the instances to which Developers can connect, by putting a Condition in the policy.
+
+In general, this approach:
+
+Tag instances that should be accessible as SSM Jumpboxes with SSM-Jumphost = True
+
+Deny management of such tags to non-administrative/provisioning users
+In an IAM policy, an explicit deny trumps any allows
+Apply an accompanying IAM Policy to all IAM Roles requiring access
+
+Example IAM Policy that permits SSM commands only to instances so-tagged, and restricts management of this Tag:
+
+```json
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "ec2:DescribeInstances"
+            ],
+            "Resource": "*"
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "ssm:SendCommand",
+                "ssm:StartSession"
+            ],
+            "Resource": "arn:aws:ec2:*:*:instance/*",
+            "Condition": {
+                "StringLike": {
+                    "ssm:resourceTag/SSM-Jumphost": "True"
+                }
+            }
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "ssm:SendCommand",
+                "ssm:StartSession"
+            ],
+            "Resource": [
+                "arn:aws:ssm:*::document/AWS-RunShellScript",
+                "arn:aws:ssm:*::document/AWS-StartSSHSession"
+            ]
+        },
+        {
+            "Effect": "Deny",
+            "Action": [
+                "ec2:DeleteTags",
+                "ec2:CreateTags"
+            ],
+            "Resource": "*",
+            "Condition": {
+                "ForAllValues:StringEquals": {
+                    "aws:TagKeys": "SSM-Jumphost"
+                }
+            }
+        }
+    ]
+}
+```
+
+# Auditing
+
+CloudTrail obviously but also via SSM
+
+![ssm session log](doc/ssm-session-log.png)
+
+This can also go to CloudWatch logs and s3
+
+# F.A.Q.s
+**Q**: Does this work for MS Remote Desktop (RDP)?
+
+**A**:  Yes you can forward port 3389 with e.g. ssh -N -L 3389:windows-hostname:3389 i-xxjumpboxxx
+
+
+**Q**: Why SSH? (via SSM)
+
+**A**: Standard way for port fwd, proxyjump, SOCKS proxy etc
+
+Simple & standard to implement
+
+AWS-documented and supported
+https://docs.aws.amazon.com/systems-manager/latest/userguide/session-manager-getting-started-enable-ssh-connections.html
+
+Can scp (can’t with just SSM)
+
+# Troubleshooting
+
+Problem|Solution
+-|-
+SSM doesn’t work at all - including web console | Confirm security groups allow SSM connection, and confirm the ec2 instance profile has required IAM policy
+|Confirm the role you’re logged into console with has permission to access SSM
+|Confirm the instance can reach the SSM endpoint (I had issues reaching boxes in a public subnet)
+SSM works in console, but you get something like ```An error occurred (InvalidInstanceId) when calling the SendCommand operation: An error occurred (TargetNotConnected) when calling the StartSession operation: <i-someinstanceID> is not connected.```| Confirm the region you are logging in as. If not explicitly specified, the aws cli will pull the default from ~/.aws/config (which might result in trying to talk to a different/nonexistent instance)
+SSM Connects, SSH Can’t auth|Check the SSH user you’re authenticating as; the scripts above use `ec2-user` which is the user for Amazon Linux. If you’re using ubuntu, the user is `ubuntu`
+SSM Connects, SSH Connects, but port forwarding hangs|If running `ssh -vvv` you’ll see something like: `OpenSSH_7.9p1, LibreSSL 2.7.3 debug1: Reading configuration data /Users/xxx/.ssh/config debug1: /Users/xxx/.ssh/config line 1: Applying options for * debug1: Reading configuration data /etc/ssh/ssh_config debug1: /etc/ssh/ssh_config line 48: Applying options for * debug1: Executing proxy command: exec aws ssm start-session --target ‘i-xxx         --document-name ‘AWS-StartSSHSession’ debug1: identity file /Users/xxx/.ssh/id_rsa type 0 debug1: identity file /Users/xxx/.ssh/id_rsa-cert type -1 debug1: Local version string SSH-2.0-OpenSSH_7.9 debug1: ssh_exchange_identification: debug1: ssh_exchange_identification: Starting session with SessionId: xxx@xxx.com-xxx debug1: ssh_exchange_identification: \033[?1034hsh-4.2$ \033[Ksh-4.2$ SSH-2.0-OpenSSH_7.9 debug1: ssh_exchange_identification: sh: SSH-2.0-OpenSSH_7.9: command not found debug1: ssh_exchange_identification: sh-4.2$` **This is most likely due to the SSM Agent being out of date**
+
+
+
+
+Further doco at the top of [the script](aws-ssm-ssh-proxycommand.sh)
---
+++ aws-ssm-ssh-proxycommand.sh
@@ -0,0 +1,267 @@
+#!/usr/bin/env bash
+#
+# Description
+# Bootstrap SSH Session to an SSM-managed instance
+# by temporarily adding a public SSH key available on the local machine (ssh-agent or in ~/.ssh)
+# 
+#
+# Installation
+#
+# First run your eye over this script to check for malicious code
+# Then run this script without arguments to automatically perform all install steps on your client:
+#
+#   curl -s 'https://raw.githubusercontent.com/intelematics/infra-utils/master/aws/aws-ssm-ec2-proxy-command.sh?token=AAI4XFMHK5WPJ5FXFTT7U226KXB6M' | bash
+#
+# It will:
+#
+# #1 Install the AWS CLI
+#   https://docs.aws.amazon.com/cli/latest/userguide/cli-chap-install.html
+#
+# #2 Install the Session Manager Plugin for the AWS CLI
+#   https://docs.aws.amazon.com/systems-manager/latest/userguide/session-manager-working-with-install-plugin.html
+#
+# #3 Install this script
+#  	- Move this script to ~/.ssh/aws-ssm-ec2-proxy-command.sh
+#  	- Make it executable (chmod +x ~/.ssh/aws-ssm-ec2-proxy-command.sh)
+#
+# #4 Update your SSH config to use this script as ProxyCommand when SSH'ing to EC2 instances:
+#  		Host i-* mi-* jumpbox
+#  			ProxyCommand ~/.ssh/aws-ssm-ec2-proxy-command.sh %h %r %p
+#			StrictHostKeyChecking no
+#
+#
+# Host Requirements
+#
+# #1 Ensure SSM Permissions of Target Instance Profile
+#
+#   https://docs.aws.amazon.com/systems-manager/latest/userguide/setup-instance-profile.html
+#
+# #2 Ensure latest SSM Agent on Target Instance
+#
+#   Is preinstalled on all amazon linux AMIs, however may needs to be updated
+#   yum install -y https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/linux_amd64/amazon-ssm-agent.rpm & service amazon-ssm-agent restart
+#   or
+#   aws ssm send-command --instance-ids i-xxxxxxxxxx --document-name AWS-UpdateSSMAgent
+#
+# #3 (optional) name the host "jumpbox"
+# Then if you "ssh jumpbox" you'll connect to it, instead of using instance IDs
+#
+#
+# Example Usage
+#
+#	ssh ec2-user@i-xxxxxxxxxx
+#	ssh ubuntu@jumpbox
+#	ssh -N -L 5432:myrds.cunh7nydpqk3.ap-southeast-2.rds.amazonaws.com:5432 ubuntu@i-xxjumpboxx
+#
+#
+# TODO
+# Possibly - replace the SSH key provisioning with ec2 instance connect
+# https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/ec2-instance-connect-set-up.html
+# Although 'neater' and using an official AWS Service,
+# this does increase requirements to having EC2 instance connect installed (scripts + sshd config AuthorizedKeysCommand)
+# And adding relevant IAM perms to the accessing role (ec2-instance-connect:SendSSHPublicKey)
+#
+################################################################################
+
+DEFAULT_SSH_PUBLIC_KEY_PATHS="${HOME}/.ssh/id_rsa.pub ${HOME}/.ssh/id_ed25519.pub"
+SSH_PUBLIC_KEY_TIMEOUT=5
+SSH_USER_DEFAULT=ec2-user
+SSH_PORT_DEFAULT=22
+THIS_SCRIPT_URL="https://raw.githubusercontent.com/intelematics/infra-utils/master/aws/aws-ssm-ec2-proxy-command.sh?token=AAI4XFL3MXRBWJLBCGJV5YC6KXQCG"
+
+# Convnience feature to enable just "ssh jumpbox" (instead of instance id)
+# We then find the first ec2 instance with name tag = "jumpbox"
+# and connect to that
+MAGIC_JUMPBOX_INSTANCE_NAME="jumpbox"
+
+
+main() {
+
+	args=($@)
+	local cmd=${args[0]-install}
+	local install_location=${args[1]-~/.ssh/aws-ssm-ec2-proxy-command.sh}
+	local ec2_instance_id=${args[0]}
+	local ssh_user=${args[1]-$SSH_USER_DEFAULT}
+	local ssh_port=${args[2]-$SSH_PORT_DEFAULT}
+
+	usage="
+Usage:\n
+  aws-ssm-ec2-proxy-command.sh install [install-location (default ~/.ssh)]\n
+  aws-ssm-ec2-proxy-command.sh i-xxxxxxx [ssh-username] [ssh-port]\n
+"
+	if [[ "install" == $cmd ]] ; then
+		echo
+		echo
+		install $install_location
+		if [[ 0 != $? ]] ; then
+			echo "Problem installing - bailing"
+			exit $?
+		fi
+		exit 0
+	else 
+		if [[ -z $ec2_instance_id ]] ; then
+			echo -e $usage
+			exit 1
+		fi
+		connect $ec2_instance_id $ssh_user $ssh_port
+		exit 0
+	fi
+}
+
+
+install_aws_cli() {
+	if [[ -n `which aws` ]] ; then
+		return
+	fi
+	echo
+	echo "Installing AWS CLI ..."
+
+	if [[ -n `which python3` ]] ; then
+		sudo pip3 install --prefix=/usr/local awscli
+	elif [[ -n `which python` ]] ; then
+		sudo pip install --prefix=/usr/local awscli
+	else
+		echo "You need python installed!"
+		exit 1
+	fi
+	echo
+}
+
+
+install_aws_cli_session_manager_plugin() {
+	if [[ -n `which session-manager-plugin` ]] ; then
+		return
+	fi
+	echo "Installing AWS CLI session-manager-plugin ..."
+	uname=`uname -a`
+	if [[ -n `echo $uname | grep Darwin` ]] ; then
+		curl "https://s3.amazonaws.com/session-manager-downloads/plugin/latest/mac/sessionmanager-bundle.zip" -o "/tmp/sessionmanager-bundle.zip"
+		unzip -d /tmp/ /tmp/sessionmanager-bundle.zip
+		sudo /tmp/sessionmanager-bundle/install -i /usr/local/sessionmanagerplugin -b /usr/local/bin/session-manager-plugin
+		rm /tmp/session-manager-plugin-bundle.zip
+	elif [[ -n `echo $uname | grep Ubuntu` || -n `echo $uname | grep "^Linux.*Microsoft"` ]] ; then
+		curl "https://s3.amazonaws.com/session-manager-downloads/plugin/latest/ubuntu_64bit/session-manager-plugin.deb" -o "/tmp/session-manager-plugin.deb"
+		sudo dpkg -i /tmp/session-manager-plugin.deb
+		rm /tmp/session-manager-plugin.deb
+	else
+		echo "Don't know how to install session-manager-plugin for your system - please do so manually then try again. Refer here for instructions: https://docs.aws.amazon.com/systems-manager/latest/userguide/session-manager-working-with-install-plugin.html"
+		exit 1
+	fi
+}
+
+
+install() {
+	local install_location=$1
+
+	install_aws_cli
+	install_aws_cli_session_manager_plugin
+
+	mkdir -p `dirname $install_location`
+
+	curl -s $THIS_SCRIPT_URL > $install_location
+	if [[ 0 != $? ]] ; then
+		echo "Couldn't download script from $THIS_SCRIPT_URL - bailing"
+		exit $?
+	fi
+	chmod +x $install_location
+
+	# Update ssh config, if needed
+	if [[ -z `grep 'Host i-\*' ~/.ssh/config` ]] ; then
+		echo "Updating ~/.ssh/config to use the script as ProxyCommand for 'ssh i-*'"
+		echo "
+Host i-* mi-* ${MAGIC_JUMPBOX_INSTANCE_NAME}
+	User ${SSH_USER_DEFAULT}
+	ProxyCommand ${install_location} %h %r %p
+	StrictHostKeyChecking no
+" >> ~/.ssh/config
+	fi
+
+	echo "Installed to ${install_location}"
+}
+
+
+get_ssh_public_key() {
+	# Try to get an public ssh key from 'ssh agent'
+	ssh_public_key="$(keys="$(ssh-add -L 2>/dev/null)" && echo $keys | head -1)"
+	if [[ -n "$ssh_public_key" ]]; then
+		ssh_public_key_source='ssh agent'
+	else
+		# Try read public ssh key from '${DEFAULT_SSH_PUBLIC_KEY_PATHS}'
+		for ssh_public_key_path in $DEFAULT_SSH_PUBLIC_KEY_PATHS; do
+			ssh_public_key="$([[ -e "${ssh_public_key_path}" ]] && cat "${ssh_public_key_path}")"
+			if [[ -n "$ssh_public_key" ]]; then
+				ssh_public_key_source="${ssh_public_key_path}"
+			fi
+		done
+	fi
+
+	# Try getting ANY ssh key in ~/.ssh
+	# If found - start ssh-agent and add it
+	if [[ -z "${ssh_public_key}" ]]; then
+		for ssh_public_key_path in $(ls ~/.ssh/*.pub 2>/dev/null); do
+			# Check we have (what looks like) the corresponding private key
+			ssh_private_key_path="$(dirname $ssh_public_key_path)/$(basename $ssh_public_key_path .pub)"
+			if [[ -e "$ssh_private_key_path" ]] ; then
+				ssh_public_key="$(cat ${ssh_public_key_path})"
+				ssh-agent
+				echo "Adding ${ssh_private_key_path} to ssh-agent ..."
+				ssh-add ${ssh_private_key_path}
+				break
+			fi
+		done
+	fi
+
+	if [[ -z "$ssh_public_key" ]]; then
+		echo "No ssh key present in ssh agent nor ~/.ssh/"
+	exit 1
+	fi
+
+	echo $ssh_public_key
+}
+
+
+get_ec2_instance_id_for_name() {
+	local instance_name=$1
+
+	instance_id=`aws ec2 describe-instances --filters "Name=tag:Name,Values=${MAGIC_JUMPBOX_INSTANCE_NAME}" --query "Reservations[].Instances[].InstanceId" --output text`
+	echo $instance_id
+}
+
+
+connect() {
+	local ec2_instance_id=$1
+	local ssh_user=$2
+	local ssh_port=$3
+	local ssh_public_key=$(get_ssh_public_key)
+
+	if [[ ${ec2_instance_id} == ${MAGIC_JUMPBOX_INSTANCE_NAME} ]] ; then
+		ec2_instance_id=`get_ec2_instance_id_for_name ${ec2_instance_id}`
+		if [[ -z "$ec2_instance_id" ]] ; then
+			echo "Couldn't get instance ID for host named '${MAGIC_JUMPBOX_INSTANCE_NAME}' - ensure you have one in your account"
+			exit 1
+		fi
+	fi
+
+	aws ssm send-command \
+		--instance-ids "${ec2_instance_id}" \
+		--document-name 'AWS-RunShellScript' \
+		--parameters commands="\"
+			sudo su
+			mkdir -p ~${ssh_user}/.ssh
+			chown -R ${ssh_user}:${ssh_user} ~${ssh_user}/.ssh
+			cd ~${ssh_user}/.ssh || exit 1
+			grep -F '${ssh_public_key}' authorized_keys || echo '${ssh_public_key} ssm-session' >> authorized_keys
+			sleep ${SSH_PUBLIC_KEY_TIMEOUT}
+			grep -v -F '${ssh_public_key}' authorized_keys > .tmp.authorized_keys
+			mv .tmp.authorized_keys authorized_keys
+		\"" \
+		--comment "grant ssh access for ${SSH_PUBLIC_KEY_TIMEOUT} seconds"
+
+	# Start SSM SSH session
+	aws ssm start-session \
+		--target "${ec2_instance_id}" \
+		--document-name "AWS-StartSSHSession" \
+		--parameters "portNumber=${ssh_port}"
+}
+
+main "$@"
